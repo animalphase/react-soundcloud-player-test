26fe66762e00a6544472fb0c375e60d8e8b89ddc
Audrey Moon <audrey@loveme.computer>
2017-08-23 03:35:20 -0500

    tried out some solutions. embedded a soundclout player

--- client/containers/App/index.js
+++ client/containers/App/index.js
@@ -18,6 +18,15 @@ export default class App extends Component {
           <label htmlFor="soundcloud-url-input">soundcloud song url</label>
           <input type="text" id="soundcloud-url-input" name="soundcloud-url" />
           <input type="submit" value="Play Song" />
+          <hr />
+          <h4>soundcloud iframe manual embed:</h4>
+          <iframe
+            width="240"
+            height="240"
+            scrolling="no"
+            frameBorder="no"
+            src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/233644433&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"
+          />
           {/*<ReactLoading type="spin" color="#444" delay="1" />*/}
         </form>
       </div>
